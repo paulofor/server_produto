559bd2764aeb5385db73fd4c28dec1572ae1e188
Paulo Alexandre <paulofore@gmail.com>
2019-04-17 21:43:32 -0300

    edicao

--- common/models/treino_academia/item-serie.js
+++ common/models/treino_academia/item-serie.js
@@ -1,6 +1,18 @@
 
 'use strict';
 
-module.exports = function(ItemSerie) {
+module.exports = function (ItemSerie) {
 
+
+    ItemSerie.SubmitCriaSeriePage = function (item, callback) {
+        console.log('ItemSerie.SubmitCriaSeriePage - input: ' , JSON.stringify(item));
+        var resultado = item;
+        callback(null, resultado);
+    };
+
+    ItemSerie.SubmitEditaItemSeriePage = function (item, callback) {
+        console.log('ItemSerie.SubmitEditaItemSeriePage - input: ' , JSON.stringify(item));
+        var resultado = item;
+        callback(null, resultado);
+    };
 };
