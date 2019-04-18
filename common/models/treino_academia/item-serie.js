d6ccfe7b68ec5e11968062c2731d7b9f2fba3dfd
Paulo Alexandre Lopes Forestieri <tr626987@oi.corp.net>
2019-04-18 11:51:07 -0300

    submits

--- common/models/treino_academia/item-serie.js
+++ common/models/treino_academia/item-serie.js
@@ -1,17 +1,39 @@
 
 'use strict';
 
+var app = require('../../../server/server');
+
 module.exports = function (ItemSerie) {
 
 
     ItemSerie.SubmitCriaSeriePage = function (item, callback) {
-        console.log('ItemSerie.SubmitCriaSeriePage - input: ' , JSON.stringify(item));
+        console.log('ItemSerie.SubmitCriaSeriePage - input: ', JSON.stringify(item));
+
+        var ds = ItemSerie.dataSource;
+        var teste = app.models.ItemSerie;
+
+        Object.getOwnPropertyNames(ds).forEach(function (val, idx, array) {
+            console.log(val + ' -> ' + ds[val]);
+        });
+        for (var item in app.dataSources.db) {
+            console.log('app.dataSources.db.', JSON.stringify(item));
+        }
         var resultado = item;
         callback(null, resultado);
     };
 
     ItemSerie.SubmitEditaItemSeriePage = function (item, callback) {
-        console.log('ItemSerie.SubmitEditaItemSeriePage - input: ' , JSON.stringify(item));
+        const transacao = app.dataSources.db.transaction;
+        await transacao(async models => {
+            const { MyModel } = models;
+            console.log(await MyModel.count()); // 0
+            await MyModel.create({ foo: 'bar' });
+            console.log(await MyModel.count()); // 1
+        });
+        console.log(await app.models.MyModel.count()); // 1
+
+
+        console.log('ItemSerie.SubmitEditaItemSeriePage - input: ', JSON.stringify(item));
         var resultado = item;
         callback(null, resultado);
     };
