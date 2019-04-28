bf66aea412a219980fe39f5e370554ee3a2cc045
Paulo Alexandre <paulofore@gmail.com>
2019-04-27 21:20:07 -0300

    evolucao

--- common/models/treino_academia/execucao-item-serie.js
+++ common/models/treino_academia/execucao-item-serie.js
@@ -1,6 +1,28 @@
 
 'use strict';
 
-module.exports = function(ExecucaoItemSerie) {
+var app = require('../../../server/server');
+
+module.exports = function (ExecucaoItemSerie) {
+
+
+
+    ExecucaoItemSerie.SubmitExecutaTreinoPage = function (item, callback) {
+        ExecucaoItemSerie.create(item, (err, result) => {
+            if (result.id) {
+                var itemsProcessed =0 ;
+                item.listaExecucaoCarga.forEach(element => {
+                    itemsProcessed++;
+                    element.execucaoItemSerieId = result.id;
+                    app.models.TreinoAcademia_ExecucaoCarga.create(element);
+                    if (itemsProcessed === item.listaExecucaoCarga.length) {
+                        callback(null, result);
+                    }
+                });
+            }
+        })
+
+    }
+
 
 };
