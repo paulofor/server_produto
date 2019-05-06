374ffae94bc20159bc1abb7181f87e0aaa25a6d2
Paulo Alexandre Lopes Forestieri <tr626987@oi.corp.net>
2019-05-06 13:27:41 -0300

    ajustes

--- common/models/treino_academia/execucao-item-serie.js
+++ common/models/treino_academia/execucao-item-serie.js
@@ -6,35 +6,36 @@ var app = require('../../../server/server');
 module.exports = function (ExecucaoItemSerie) {
 
 
-
+ 
+    
     ExecucaoItemSerie.SubmitExecutaTreinoPage = function (item, callback) {
-
+        var teveErro = 0;
         ExecucaoItemSerie.create(item, (err, result) => {
-            if (err) {
+             if (err) {
                 callback(err,null);
                 return;
             }
             if (result.id) {
                 var itemsProcessed =0 ;
                 item.listaExecucaoCarga.forEach(element => {
-                    itemsProcessed++;
                     element.execucaoItemSerieId = result.id;
- 
                     app.models.TreinoAcademia_ExecucaoCarga.create(element, (err,result) => {
+                        itemsProcessed++;
                         if (err) {
+                            teveErro = 1;
                             callback(err,null);
                             return;
                         };
-                        if (itemsProcessed === item.listaExecucaoCarga.length) {
-                            callback(null, result);
+                        if (itemsProcessed == item.listaExecucaoCarga.length && teveErro==0) {
+                            callback(null, item);
                         }
                     });
                    
                 });
             }
         })
-
     }
+ 
 
 
 };
