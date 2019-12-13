f89ee677a52108437775afc10d4f3683485ba4f4
Paulo Alexandre <paulofore@gmail.com>
2019-12-13 20:59:12 -0300

    https funcionando no lab

--- versao_lab/server/server.js
+++ versao_lab/server/server.js
@@ -1,13 +1,18 @@
+'use strict';
+
 var loopback = require('loopback');
 var boot = require('loopback-boot');
 var loopbackSSL = require('loopback-ssl');
- 
+
 var app = module.exports = loopback();
- 
+
+// Add Counts Mixin to loopback
 require('loopback-counts-mixin')(app);
 
+var app = module.exports = loopback();
+ 
 boot(app, __dirname, function(err) {
   if (err) throw err;
 });
- 
+
 return loopbackSSL.startServer(app);
