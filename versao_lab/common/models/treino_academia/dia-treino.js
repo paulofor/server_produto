3997261872b82ad5d5c351173d0280a232a4f098
Paulo Alexandre <paulofore@gmail.com>
2019-12-17 22:10:47 -0300

    versao com fechamento treino

--- versao_lab/common/models/treino_academia/dia-treino.js
+++ versao_lab/common/models/treino_academia/dia-treino.js
@@ -5,6 +5,18 @@ module.exports = function(DiaTreino) {
 
     DiaTreino.FinalizaDia = function (IdDiaTreino, callback) {
         console.log('IdDiaTreino:' , IdDiaTreino);
-        callback(null,null);
+        var sql1 = "update TreinoAcademia_DiaTreino set concluido = 1 where id = '" + IdDiaTreino + "' ";
+        var sql2 = "update TreinoAcademia_SerieTreino set dataUltimaExecucao = UTC_TIMESTAMP() " + 
+                " where id = ( select serieTreinoId from TreinoAcademia_DiaTreino " +
+                " where id = '" + IdDiaTreino + "')";
+
+        var ds = DiaTreino.dataSource;
+        ds.connector.query(sql1, (err1, result1) => {
+            if (err1) {
+                callback(err1, null);
+                return;
+            }
+            ds.connector.query(sql2,callback)
+        });
     }
 };
