ce514f1f2b1243f6970f37e048cd634ba2aeb3a3
Paulo Alexandre <paulofore@gmail.com>
2020-01-18 22:45:38 -0300

    evolucao

--- versao_atual/common/models/cosmetic/oportunidade-dia.js
+++ versao_atual/common/models/cosmetic/oportunidade-dia.js
@@ -1,6 +1,16 @@
 
 'use strict';
 
-module.exports = function(OportunidadeDia) {
+module.exports = function (OportunidadeDia) {
 
+
+    /**
+    * chamada pelos aplicativos
+    * @param {Function(Error, array)} callback
+    */
+
+    OportunidadeDia.ListaApp = function (callback) {
+        var lista;
+        OportunidadeDia.find({"order" : "dataUltimaPrecoAnterior desc"},callback);
+    };
 };
